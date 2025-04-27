22afb27e6cf7fe4fd97b7b863ebddd725f9fe238
JKkims <jisung.kim78@gmail.com>
2025-04-27 11:00:18 +0900

    modified:   src/app/lib/db.js

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST_TEST,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: process.env.DB_USER_TEST,
-  password: process.env.DB_PASSWORD_TEST,
-  database: process.env.DB_DATABASE_TEST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   connectionLimit: 30, // 연결 풀 크기 설정 (선택 사항)
 });
 
